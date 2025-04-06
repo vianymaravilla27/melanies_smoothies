e023fa96604bcbaed8a19c357b4c276539aceb30
Vianey Maravilla <112917745+vianymaravilla27@users.noreply.github.com>
2025-04-06 11:30:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+cnx = st.connection("snowflake", type="snowflake")
+session = cnx.session()
 
 # Título e instrucciones
 st.title("🥤 Customize Your Smoothie!")
