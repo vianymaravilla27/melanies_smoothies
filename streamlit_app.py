531fc5032847eb907f265ca4626e44d229f6f30d
Vianey Maravilla <112917745+vianymaravilla27@users.noreply.github.com>
2025-04-06 11:57:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,9 +53,16 @@ if st.button("Submit Order"):
 
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
+if igredients_list:
+  ingredients_string = ''
+
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ' '
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
+
 
 
 
