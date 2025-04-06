0ea8d71b99ca0a609ebbf65f1adbdc0fa728b4cb
Vianey Maravilla <112917745+vianymaravilla27@users.noreply.github.com>
2025-04-06 11:43:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,8 @@ if st.button("Submit Order"):
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = true)
 
 
 
