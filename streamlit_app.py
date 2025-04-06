65d9f6bcbe501ed8faab2d4ab3662ed7a43b0d4d
Vianey Maravilla <112917745+vianymaravilla27@users.noreply.github.com>
2025-04-06 12:11:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,6 +65,10 @@ if ingredients_list:
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
 
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width = True)
+st.stop
+
 
 
 
