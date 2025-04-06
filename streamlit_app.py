498674359f2d8db5a8949ec0a6d78197611e0781
Vianey Maravilla <112917745+vianymaravilla27@users.noreply.github.com>
2025-04-05 23:55:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 # Import python packages
 import streamlit as st
-
-
-
-
+from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
@@ -11,19 +8,13 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 
 from snowflake.snowpark.functions import col
 
-
-my_dataframe = session.table("smoothies.publitry:
-    frutas = session.table("smoothies.public.fruit_options").select("FRUIT_NAME").collect()
-    st.write("🥭 Frutas disponibles:", frutas)
-except Exception as e:
-    st.error(f"❌ Error al acceder a la tabla fruit_options: {e}")
-c.fruit_options").select(col('FRUIT_NAME'))
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe)
-
 if ingredients_list:
     ingredients_string = ''
     
@@ -40,9 +31,6 @@ if ingredients_list:
     if time_to_insert: 
        session.sql(my_insert_stmt).collect() 
        st.success('¡Tu batido está pedido!', icon="✅")
-
-cnx = st.connection("snowflake")
-session = cnx.session()
 
        
 
