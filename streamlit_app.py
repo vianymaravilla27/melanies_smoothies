72ce89e46e369968e001c93198491a2e97c59d90
Vianey Maravilla <112917745+vianymaravilla27@users.noreply.github.com>
2025-04-05 22:59:50 -0600

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
@@ -15,9 +15,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe)
-
-
-
 
 if ingredients_list:
     ingredients_string = ''
@@ -35,6 +32,9 @@ if ingredients_list:
     if time_to_insert: 
        session.sql(my_insert_stmt).collect() 
        st.success('¡Tu batido está pedido!', icon="✅")
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
        
 
