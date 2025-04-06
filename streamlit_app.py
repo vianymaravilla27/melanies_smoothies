80051471f519610e6de751982a1dc488b45f2e9a
Vianey Maravilla <112917745+vianymaravilla27@users.noreply.github.com>
2025-04-06 12:19:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,18 +54,6 @@ if st.button("Submit Order"):
         st.success("Your Smoothie is ordered!", icon="✅")
 
 
-
-
-if ingredients_list:
-  ingredients_string = ''
-
-  for fruit_chosen in ingredients_list:
-    ingredients_string += fruit_chosen + ' '
-    st.subheader(fruit_chosen + 'Nutrition Information')
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
-    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
-
-
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width = True)
 #st.stop
@@ -74,8 +62,21 @@ pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
-ingredients_list = st.multiselect()
+ingredients_list = st.multiselect(
+  'Choose up to 5 ingredients:'
+  , my_dataframe
+  , max_selections = 5
+)
 
+if ingredients_list:
+  ingredients_string = ''
 
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ' '
+
+    search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    st.write('The search value for', fruit_chosen, 'is', search_on,'.')
+    st.subheader(fruit_chosen + 'Nutrition Information')
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
 
 
